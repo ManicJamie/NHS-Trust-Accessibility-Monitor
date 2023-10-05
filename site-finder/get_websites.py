75ed262df3809cd042c0d26e3892d6c2ea270234
Jamie <jamiebloomfield8@gmail.com>
2023-10-05 18:56:28 +0100

    Adjust get_websites script

--- site-finder/get_websites.py
+++ site-finder/get_websites.py
@@ -8,31 +8,37 @@ import requests
 from bs4 import BeautifulSoup
 import re, csv, logging
 
-logging.getLogger().setLevel(logging.WARNING)
+logging.getLogger().setLevel(logging.INFO)
 
 LIST_URL = "https://www.england.nhs.uk/publication/nhs-provider-directory/"
 
-site = BeautifulSoup(requests.get(LIST_URL).content, features="lxml")
-main = site.find(name="section", attrs={"id": "main-content"})
+def scrape_list_site(url):
+    site = BeautifulSoup(requests.get(url).content, features="lxml")
+    main = site.find(name="section", attrs={"id": "main-content"})
 
-hrefs = filter(lambda x: x is not None, [x.findChild(name="a") for x in main.find_all(name="li")])
+    hrefs = filter(lambda x: x is not None, [x.findChild(name="a") for x in main.find_all(name="li")])
+    # This is a dirty scrape, including an additional element from the end of the page - this will trigger a warning.
 
-failed = {}
+    failed = {}
 
-with open("urls.csv", "w") as f:
-    writer = csv.DictWriter(f, ["Trust", "URL"])
+    with open("urls.csv", "w") as f:
+        writer = csv.DictWriter(f, ["Trust", "URL"])
 
-    for name, index_url in {tag.text: tag.get("href") for tag in hrefs}.items():
-        logging.info(f"Fetching {name} from {index_url}")
-        content = BeautifulSoup(requests.get(index_url).content, features="lxml")
+        for name, index_url in {tag.text: tag.get("href") for tag in hrefs}.items():
+            logging.info(f"Fetching {name} from {index_url}")
+            content = BeautifulSoup(requests.get(index_url).content, features="lxml")
 
-        websiteLabel = content.find(re.compile("^h[1-6]$"), string="Website")
-        if websiteLabel is None:
-            logging.warning(f"Failed to find website at {index_url}")
-            failed[name] = index_url
-            continue
-        websiteURLLabel = websiteLabel.find_next_sibling("p").find("a")
-        writer.writerow({"Trust": name, "URL": websiteURLLabel.get("href")})
+            websiteLabel = content.find(re.compile("^h[1-6]$"), string="Website")
+            if websiteLabel is None:
+                logging.warning(f"Failed to find website at {index_url}")
+                failed[name] = index_url
+                continue
 
-if len(failed) > 0:
-    logging.warning(f"Failed to find websites for the following list items: {failed}")
+            websiteURLLabel = websiteLabel.find_next_sibling("p").find("a")
+            writer.writerow({"Trust": name, "URL": websiteURLLabel.get("href")})
+
+    if len(failed) > 0:
+        logging.warning(f"Failed to find websites for the following list items: {failed}")
+
+if __name__ == "__main__":
+    scrape_list_site(LIST_URL)
